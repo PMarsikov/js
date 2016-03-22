d0c37306bedd9be883524752e8787e8980dab9bf
Pavel Marsikov <MarsikovPavel@mail.ru>
2016-03-22 10:53:26 +0300

    Program Structure. Task Program Structure

---
+++ task1.js
@@ -0,0 +1,18 @@
+﻿//var size =  prompt('Set table size:', 8);
+var size = 8;
+var board = "";
+
+
+for (var i = 0; i < size; i++){
+	for (var j = 0; j < size; j++){
+		if ((i + j) % 2 == 0 ){	
+			board += " ";
+		} 
+		else {
+			board += "#";
+		}
+		
+	}
+	board += "\n";
+}
+console.log(board);
